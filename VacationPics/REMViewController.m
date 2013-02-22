688c38f7e479d0eed4b25dcbe73c005ea13c2a6a
Richard Millet <milletdesigns@me.com>
2013-02-21 18:19:08 -0800

    Implemented image scrolling and pinch and zoom.

--- VacationPics/REMViewController.m
+++ VacationPics/REMViewController.m
@@ -8,8 +8,9 @@
 
 #import "REMViewController.h"
 
-@interface REMViewController ()
-
+@interface REMViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @end
 
 @implementation REMViewController
@@ -18,12 +19,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
+	self.scrollView.contentSize = self.imageView.image.size;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIScrollViewDelegate method
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+	return self.imageView;
 }
 
 @end
